20ddee368f2efced5fba000d607725956edb9b67
Borna Koružnjak <borna@getizzy.com>
2016-11-25 10:30:29 +0100

    - finished graphical concept

--- app/src/main/java/bkoruznjak/from/hr/androidnarodniconcept/FourthTestActivity.java
+++ app/src/main/java/bkoruznjak/from/hr/androidnarodniconcept/FourthTestActivity.java
@@ -1,7 +1,10 @@
 package bkoruznjak.from.hr.androidnarodniconcept;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -10,12 +13,19 @@ import bkoruznjak.from.hr.androidnarodniconcept.databinding.ActivityFourthTestBi
 
 public class FourthTestActivity extends AppCompatActivity implements SurfaceHolder.Callback, FudgeClickListener {
 
+    private final int LOGO_IMAGES[] = {R.drawable.img_blue_logo, R.drawable.img_purple_logo, R.drawable.img_green_logo, R.drawable.img_orange_logo, R.drawable.img_gray_logo, R.drawable.img_red_logo};
     ActivityFourthTestBinding binding;
+    TransitionDrawable mTransitionDrawable;
+    Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_fourth_test);
+        mTransitionDrawable = new TransitionDrawable(new Drawable[]{getResources().getDrawable(LOGO_IMAGES[0]),
+                getResources().getDrawable(LOGO_IMAGES[0])});
+        binding.imgLogo.setImageDrawable(mTransitionDrawable);
+        mHandler = new Handler();
     }
 
     @Override
@@ -63,7 +73,23 @@ public class FourthTestActivity extends AppCompatActivity implements SurfaceHold
     public void onFudgePressed(int fudgeId) {
         if (binding.surfaceViewCustom.isRunning) {
             binding.surfaceViewCustom.growFudge(fudgeId);
+
+            switchOutLogo(fudgeId);
+
+
         }
+
+    }
+
+    public void switchOutLogo(int logoId) {
+        Drawable oldDrawable = mTransitionDrawable.getDrawable(1);
+        mTransitionDrawable = new TransitionDrawable(new Drawable[]{
+                oldDrawable,
+                getResources().getDrawable(LOGO_IMAGES[logoId - 1])
+        });
+        binding.imgLogo.setImageDrawable(mTransitionDrawable);
+        mTransitionDrawable.startTransition(2000);
+
 
     }
 
